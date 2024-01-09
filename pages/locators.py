f45891d11232322892376015945c82cea880968d
totSamiy73 <dmitriy73a@mail.ru>
2024-01-09 10:33:19 +0300

    Pros of inheritance

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    pass
+    # LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, "login_form")
@@ -18,3 +19,7 @@ class ProductPageLocator:
     MESSAGE_CART_COST = (By.XPATH, '//*[@id="messages"]/div[3]')
     CART_PRICE = (By.CSS_SELECTOR, 'div.alert > div > p:nth-child(1) > strong')
     PRICE_PRODUCT = (By.CSS_SELECTOR, '.product_main > p.price_color')
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
